1de2deadec8e64ee8bfcfb08083cef420cc199f7
Mauro Castro <maurocastruli@gmail.com>
2025-02-11 10:49:39 -0300

    score signals

--- mob.gd
+++ mob.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var player
 var health = 3
+signal mob_muere
+
 func _ready():
 	player = get_node("/root/Game/Player")
 	%Slime.play_walk()
@@ -18,8 +20,15 @@ func take_damage():
 	health -= 1
 		
 	if health == 0:
+		mob_muere.emit()
 		queue_free()
 		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
 		var smoke = SMOKE_SCENE.instantiate()
 		get_parent().add_child(smoke)
 		smoke.global_position = global_position
+
+	
+
+
+func _on_mob_muere() -> void:
+	print("señal muerte")
--- survivors-game.gd
+++ survivors-game.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
+var GameScore = 0
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
+	new_mob.mob_muere.connect(Score_increment)
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
-
-
+	
+	
+func _ready():
+	var GameScore = 0
+	Score_update(GameScore)
+	%GameOver.visible = false
+	get_tree().paused = false
 func _on_mob_timer_timeout():
 	spawn_mob()
 
@@ -15,3 +22,13 @@ func _on_mob_timer_timeout():
 func _on_player_health_depleted():
 	%GameOver.visible = true
 	get_tree().paused = true
+
+func Score_update(GameScore):
+	%ScoreLabel.text = "Score : " + str(GameScore)
+
+func Score_increment():
+	GameScore += 1
+	Score_update(GameScore)
+
+func _on_button_pressed():
+	get_tree().reload_current_scene()
