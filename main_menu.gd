56b4b161f0a2ee98d6415e696611011dc36076be
Mauro Castro <maurocastruli@gmail.com>
2025-02-12 16:31:34 -0300

    player name input to json

--- main_menu.gd
+++ main_menu.gd
@@ -3,3 +3,5 @@ extends Control
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://survivors-game.tscn")
+	Global.playerNAME = %name_input.text
+	print("jugador = " , Global.playerNAME)
